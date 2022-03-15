356c8cdbeb11b55a9794faf6baee0c55aed03a9f
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-14 21:09:56 -0400

    commit tree 287a981509adda36c8797a4912524286ebc1e03a

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def happy_new_year
   # your code here
+  i = 10
+  while i>0
+    puts i
+    if i == 1 then puts "Happy New Year!"
+    end
+    i -= 1
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +24,20 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  chars = str.split('')
+  reverse = ""
+  i = chars.length - 1
+  while i >= 0
+    reverse += chars[i]
+    i-=1
+  end
+  reverse
 end
+
